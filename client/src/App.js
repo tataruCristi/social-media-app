b935689e5c637cf18163855478a3c16c69e40311
Tataru Cristi <tataru.cristi5@gmail.com>
2024-02-28 21:05:49 +0200

    added routing on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
 
 function App() {
-  return <div className="App">App</div>;
+  const isAuth = true;
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/home"
+            element={isAuth ? <HomePage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/profile/:id"
+            element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { id } = useParams();
+
+  return <div>ProfilePage and id is {id}</div>;
 };
 
 export default ProfilePage;
